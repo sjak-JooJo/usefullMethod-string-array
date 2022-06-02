661e1c07fa043ef132c66dea7746e65233d46232
JooJo <sjak0101@gmail.com>
2022-06-02 02:22:30 +0100

    final commit

---
+++ try-catch.js
@@ -0,0 +1,16 @@
+const name = 'ami';
+const student = undefined;
+
+/* for(let i = 0; i < 10 i++){
+
+}
+name = 'tumi';
+console.log(name); */
+
+try{
+    name = 'tumi';
+}
+catch(ex){
+    console.log('Getting Error',ex);
+}
+console.log(name);
